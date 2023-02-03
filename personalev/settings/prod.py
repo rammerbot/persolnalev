df6242982d71c9646f485c13445ecc7f1a7abe2a
RammerBot <123994694+rammerbot@users.noreply.github.com>
2023-02-03 09:44:44 -0500

    static_files en produccion
    se agrego la dirreccion de los archivos staticos para la carga de los css en produccion

--- personalev/settings/prod.py
+++ personalev/settings/prod.py
@@ -6,7 +6,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -28,5 +28,6 @@ DATABASES = {
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
